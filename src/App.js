eff45c9993bf992b08b16074b39e74fa6cf1e46a
jonddon <odeyjohn4u@gmail.com>
2021-11-11 20:57:40 +0100

    deleted test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
+import * as ethers from 'ethers'
+import { useEthers } from '@usedapp/core'
 import './App.css';
+import xtmcClaimAbi from './abi/xtmcClaimAbi.json'
+import { useState } from 'react';
+//Get the deployed Contract Address on BSC
+const xtmcClaimAddress = "0xb89998FecfC6Fb3E2C58b464F77Fca159Fda9a5A"
+
 
 function App() {
+
+  const { activateBrowserWallet, account, deactivate, chainId
+  } = useEthers()
+  const [Errors, setErrors] = useState("");
+  const [Success, setSuccess] = useState("")
+
+  const claim = async () => {
+    if (typeof window.ethereum !== 'undefined') {
+      const provider = new ethers.providers.Web3Provider(window.ethereum)
+      const signer = provider.getSigner()
+      const contract = new ethers.Contract(xtmcClaimAddress, xtmcClaimAbi, signer)
+      try {
+        const data = await contract.claim()
+        // console.log('data: ', data)
+        setSuccess("Successfully Claimed your Airdrop")
+      } catch (errors) {
+        setErrors(errors.data.message)
+        // console.log("Error: ", errors.data.message)
+      }
+    }
+    // console.log("claimed called");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container-fluid mt-2'>
+      <div className="row">
+        <div className="col-lg-9 col-sm-6 col-md-8">
+          <h1 className='title'>
+            XTMC SWAP
+          </h1>
+        </div>
+        <div className="col-lg-3 col-sm-6 col-md-4">
+          {!account && <button className='btn btn-custom' onClick={activateBrowserWallet}> Connect </button>}
+          {account && <div>
+            <span className='account'>{account.slice(0, 8)} </span>
+            <button className='btn btn-custom' onClick={deactivate}> Disconnect </button>
+          </div>}
+
+        </div>
+        <div className="col-lg-12">
+          {chainId !== 97 && <div className="alert alert-danger text-center" role="alert">
+            Please connect to Binance smart chain
+          </div>}
+          {Errors !== "" && <div className="alert alert-danger text-center" role="alert">
+            {Errors}
+          </div>}
+
+          {Success !== "" && <div className="alert alert-danger text-center" role="alert">
+            {Success}
+          </div>}
+        </div>
+      </div>
+
+      <div className="row full-height">
+        <div className="col-lg-10 offset-lg-1 text-center">
+          <h1 className='title py-4'>
+            Claim Your Airdrop
+          </h1>
+          <button className='btn btn-custom px-4' onClick={claim} > Claim </button>
+        </div>
+      </div>
+
     </div>
-  );
+  )
 }
 
 export default App;
